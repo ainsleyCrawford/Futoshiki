5c69d13148f662e87ea4f247537e6acf28a6004a
Ainsley Crawford <ACrawford@spartaglobal.com>
2019-09-09 12:04:33 +0100

    NumberValidation functioning

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,9 +26,10 @@ namespace Futoshiki
             InitializeComponent();
         }
 
-        private void OneTwo_TextChanged(object sender, TextChangedEventArgs e)
+        private void NumberValidation(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^1-4]");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
