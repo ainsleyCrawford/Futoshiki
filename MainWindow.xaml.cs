7a0bb0aec74f5470be6550803da451bacd16616f
Ainsley Crawford <ACrawford@spartaglobal.com>
2019-09-09 15:46:40 +0100

    Hard-coded puzzle

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,12 +24,44 @@ namespace Futoshiki
         public MainWindow()
         {
             InitializeComponent();
+            
         }
-
         private void NumberValidation(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^1-4]");
             e.Handled = regex.IsMatch(e.Text);
         }
+        private void VerifyAnswer(object sender, RoutedEventArgs e)
+        {
+            int oneOne = 2, twoOne = 4, threeOne = 1, fourOne = 3;
+            int oneTwo = 1, twoTwo = 3, threeTwo = 4, fourTwo = 2;
+            int oneThree = 4, twoThree = 2, threeThree = 3, fourThree = 1;
+            int oneFour = 3, twoFour = 1, threeFour = 2, fourFour = 4;
+
+            int oneOneInput, twoOneInput, threeOneInput, fourOneInput;
+            int oneTwoInput, twoTwoInput, threeTwoInput, fourTwoInput;
+            int oneThreeInput, twoThreeInput, threeThreeInput, fourThreeInput;
+            int oneFourInput, twoFourInput, threeFourInput, fourFourInput;
+
+            oneOneInput = Convert.ToInt32(OneOne.Text); twoOneInput = Convert.ToInt32(TwoOne.Text); threeOneInput = Convert.ToInt32(ThreeOne.Text); fourOneInput = Convert.ToInt32(FourOne.Text);
+            oneTwoInput = Convert.ToInt32(OneTwo.Text); twoTwoInput = Convert.ToInt32(TwoTwo.Text); threeTwoInput = Convert.ToInt32(ThreeTwo.Text); fourTwoInput = Convert.ToInt32(FourTwo.Text);
+            oneThreeInput = Convert.ToInt32(OneThree.Text); twoThreeInput = Convert.ToInt32(TwoThree.Text); threeThreeInput = Convert.ToInt32(ThreeThree.Text); fourThreeInput = Convert.ToInt32(FourThree.Text);
+            oneFourInput = Convert.ToInt32(OneFour.Text); twoFourInput = Convert.ToInt32(TwoFour.Text); threeFourInput = Convert.ToInt32(ThreeFour.Text); fourFourInput = Convert.ToInt32(FourFour.Text);
+
+            if (oneOneInput == oneOne & twoOneInput == twoOne & threeOneInput == threeOne & fourOneInput == fourOne
+                & oneTwoInput == oneTwo & twoTwoInput == twoTwo & threeTwoInput == threeTwo & fourTwoInput == fourTwo
+                & oneThreeInput == oneThree & twoThreeInput == twoThree & threeThreeInput == threeThree & fourThreeInput == fourThree
+                & oneFourInput == oneFour & twoFourInput == twoFour & threeFourInput == threeFour & fourFourInput == fourFour)
+            {
+                MessageBox.Show("Congratulations!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong answer. Keep trying!");
+            }
+        }
+
+
+
     }
 }
